0aab3b2d16d3e142721918a7fd5f3f96c878a711
r0o0 <rayeoh7@gmail.com>
2022-08-21 21:32:51 +0900

    Refactor Heading component
    - Add `color` props

--- packages/components/src/Heading/Heading.css.ts
+++ packages/components/src/Heading/Heading.css.ts
@@ -1,19 +1,19 @@
-import { recipe } from '@vanilla-extract/recipes';
+import { style } from '@vanilla-extract/css';
+import { createSprinkles, defineProperties } from '@vanilla-extract/sprinkles';
 
 import { vars } from '../styles/global.css';
 
-export const headingStyle = recipe({
-  base: {
-    fontFamily: vars.fonts.display.family,
-    margin: 0,
-    color: 'inherit',
-  },
-  variants: {
-    size: {
-      1: { fontSize: vars.fonts.display.size[1] },
-      2: { fontSize: vars.fonts.display.size[2] },
-      3: { fontSize: vars.fonts.display.size[3] },
-      4: { fontSize: vars.fonts.display.size[4] },
-    },
-  },
+export const headingStyle = style({
+  fontFamily: vars.fonts.display.family,
+  margin: 0,
 });
+
+const headingProperties = defineProperties({
+  properties: {
+    fontSize: vars.fonts.display.size,
+    lineHeight: vars.fonts.display.lineHeight,
+  },
+  shorthands: { size: ['fontSize'] },
+});
+
+export const headingSprinkles = createSprinkles(headingProperties);
--- packages/components/src/Heading/Heading.types.ts
+++ packages/components/src/Heading/Heading.types.ts
@@ -1,10 +1,19 @@
+import { HTMLAttributes, ReactNode } from 'react';
+
+import { FontSprinkles } from '../styles/fonts.css';
+
+import { headingSprinkles } from './Heading.css';
+
+type HeadingSprinkles = Parameters<typeof headingSprinkles>[0];
+
 export type HeadingTagName = Extract<
   keyof HTMLElementTagNameMap,
   'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
 >;
 
-export interface HeadingProps extends React.HTMLAttributes<HTMLHeadingElement> {
-  heading: React.ReactNode;
+export interface HeadingProps extends HTMLAttributes<HTMLHeadingElement> {
+  heading?: ReactNode;
   as?: HeadingTagName;
-  size?: 1 | 2 | 3 | 4;
+  size?: HeadingSprinkles['size'];
+  color?: FontSprinkles['color'];
 }
