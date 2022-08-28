36770b7aad5359345035af1c7e85751a75fc8ba4
r0o0 <rayeoh7@gmail.com>
2022-08-28 11:56:02 +0900

    Fix gnb not showing in needed pages

--- packages/app/src/pages/App/App.tsx
+++ packages/app/src/pages/App/App.tsx
@@ -1,18 +1,11 @@
-import { useMemo } from 'react';
-import { Outlet, useLocation } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 
 import Nav from './Nav/Nav';
 
 function App() {
-  const location = useLocation();
-  const showNav = useMemo(
-    () => location.pathname !== '/login' && location.pathname !== '/intro',
-    [location],
-  );
-
   return (
     <>
-      {showNav && <Nav />}
+      <Nav />
       <Outlet />
     </>
   );
